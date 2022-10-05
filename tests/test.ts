624d30820185549fdd80b5645a7c2c0a65eb171d
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-10-05 22:49:17 +0900

    test: add

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,28 @@
 import { CssPutter } from "../lib/index";
 
 describe("CssPutter", () => {
-  const number = 1
-  it('hoge' , () => {
-    expect(number).toBe(1)
+  it('Rules head have white spaces ignored', () => {
+    const cssPutter = new CssPutter()
+    const css = `
+    test_id {
+      color: blue;
+    }
+    .test_class {
+      color: red;
+    }`
+    const rules = cssPutter.parse(css)
+    expect(rules).toHaveLength(0)
+  })
+
+  it('Expect Rules', () => {
+    const cssPutter = new CssPutter()
+    const css = `test_id {
+  color: blue;
+}
+.test_class {
+  color: red;
+}`
+    const rules = cssPutter.parse(css)
+    expect(rules).toHaveLength(2)
   })
 });
